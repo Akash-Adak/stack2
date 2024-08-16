b0497a63afabedaf2e120522442d45b5d7fdaedb
Akash Adak <161819740+Akash-Adak@users.noreply.github.com>
2024-08-16 21:18:52 +0530

    Update StackUsingLinkedListBetter.c

--- StackUsingLinkedListBetter.c
+++ StackUsingLinkedListBetter.c
@@ -1,87 +1,79 @@
 #include<stdio.h>
 #include<stdlib.h>
-
-struct Node
-{
+#include<stdbool.h>
+struct Node{
 	int data;
 	struct Node* next;
 };
 
 struct Node* top = NULL;
 struct Node* temp;
-int count = 0;
-
-void display()
-{
+bool isEmpty(){
+    return top==NULL;
+}
+void display(){
 	temp = top;
-	if(count == 0)
-	{
+	if(isEmpty()){
 		printf("\nStack is Empty!\n");
 		return;
 	}
-	else
-	{
-		printf("\nThere are currently %d items:-\n", count);
-		while(temp != NULL)
-		{
-			printf("%d ", temp->data);
+	else{
+		printf("\nThere are currently items:-\n");
+		while(temp != NULL){
+			printf("%d\n", temp->data);
 			temp = temp->next;
 		}	
 		printf("\n");
 	}
 }
 
-void push(int el)
-{
-	if(top == NULL)
-	{
+void push(){
+    int n;
+    printf("\nElement to be inserted: ");
+	scanf("%d", &n);
+	if(isEmpty()){
 		top = (struct Node*) malloc(sizeof(struct Node*));
-		top->data = el;
+		top->data = n;
 		top->next = NULL;
 	}
-	else
-	{
+	else{
 		temp = (struct Node*) malloc(sizeof(struct Node*));
 		temp->next = top;
-		temp->data = el;
+		temp->data = n;
 		top = temp;
 	}
-	count++;
-	display();
 }
 
-void pop()
-{
+void pop(){
 	temp = top;
-	if(top == NULL)
-	{
+	if(isEmpty()){
 		printf("\nStack is empty!\n");
 		exit(1);
 	}
-	else
+	else{
 		temp = temp->next;
 	
 	printf("\nItem to be removed is %d.", top->data);
 	free(top);
 	top = temp;
-	count--;
-	display();
+    }
+}
+void peek(){
+    if(isEmpty()){
+        printf("\nstack is empty\n");
+    }else{
+         printf("peeked elememt is=%d",top->data);
+    }
 }
 
-int main()
-{
+int main(){
 	int choice;
-	int el;
-	while(1)
-	{
-		printf("\n1.Push\n2.Pop\n3.Display\nChoice: ");
+	do{
+		printf("\n1.Push\n2.Pop\n3.Display\n4.peek\n5.Exit\nChoice: ");
 		scanf("%d", &choice);
-		switch(choice)
-		{
+		switch(choice)	{
 			case 1:
-				printf("\nElement to be inserted: ");
-				scanf("%d", &el);
-				push(el);
+				push();
 				break;
 			case 2:
 				pop();
@@ -89,9 +81,12 @@ int main()
 			case 3:
 				display();
 				break;
+                case 4:
+                peek();
+                break;
 			default:
 				printf("\nInvalid Input!\n");			
 		}
-	}
+	}while(choice!=5);
 	return 0;
 }
