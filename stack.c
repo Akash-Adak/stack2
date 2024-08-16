ecd1804d6b4a584a03f7423e8ada676957bf33ca
Akash Adak <161819740+Akash-Adak@users.noreply.github.com>
2024-08-16 20:50:28 +0530

    Update stack.c

--- stack.c
+++ stack.c
@@ -1,30 +1,34 @@
 #include<stdio.h>
+#include<stdbool.h>
 #define MAXSIZE 10
 int stack[MAXSIZE],top=-1;
+bool isFull(){
+    return top==MAXSIZE-1;
+}
+bool isEmpty(){
+    return top==-1;
+}
 void push()
 {
     int n;
-    if(top==MAXSIZE-1)
+    if(isFull())
     {
         printf("\n stack is overflow");
-    }
-    else
-    {
-        printf("enter a element=");
+    }else{
+     printf("enter a element=");
         scanf("%d",&n);
         stack[++top]=n;
     }
+        
 }
 void display()
 {
     int i;
-    if(top==-1)
+    if(isEmpty())
     {
         printf("stack is empty");
-    }
-    else 
-    {
-      printf(" enter of stack=");
+    }else{
+      printf(" enter of stacks are=");
       for(i=top;i>=0;i--)
       {
         printf("\n%d",stack[i]);
@@ -35,7 +39,7 @@ void display()
 void pop()
 {
     int n;
-    if(top==-1)
+    if(isEmpty())
     {
         printf("stack is empty");
     }
@@ -48,7 +52,7 @@ void pop()
 }
 void peek()
 {
-    if(top==-1){
+    if(isEmpty()){
         return;
     }
     printf("the peeked element is =%d",stack[top]);
@@ -58,9 +62,7 @@ void main()
     int choice;
     do
     {        
-       printf("\n           stack          \n");
        printf("\n1.push\n2.pop\n3.Display\n4.Peek\n5.Exit");
-      printf("      \n         ");
       printf("  \nenter your choice=");
       scanf("%d",&choice);
         switch (choice)
